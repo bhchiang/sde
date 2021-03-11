f99fe343f281de846966cc4e66b4fc23229d076b
Bryan Chiang <bryanhpchiang@gmail.com>
2021-03-11 04:05:30 -0800

    DeformConv v0 done

--- deform.py
+++ deform.py
@@ -100,14 +100,19 @@ class DeformableConv(nn.Module):
                     """Retrieve offset pixel values
                     _pixel_offset = (2, )
                     """
+
                     dy, dx = _pixel_offset
                     _rx, _ry = _y + dy, _x + dx
                     x0, y0 = jnp.array((_rx, _ry), jnp.int32)
                     x1, y1 = x0 + 1, y0 + 1
 
                     # Clip to the bounds of the input image
-                    y0, y1 = jnp.clip((y0, y1), a_min=0, a_max=in_h - 1)
-                    x0, x1 = jnp.clip((x0, x1), a_min=0, a_max=in_w - 1)
+                    y0, y1 = jnp.clip(jnp.array([y0, y1]),
+                                      a_min=0,
+                                      a_max=in_h - 1)
+                    x0, x1 = jnp.clip(jnp.array([x0, x1]),
+                                      a_min=0,
+                                      a_max=in_w - 1)
 
                     # Get pixels
                     p0 = _volume[y0, x0]
@@ -120,59 +125,95 @@ class DeformableConv(nn.Module):
                     w1 = (y1 - y) * (_rx - x0)  # y0, x1
                     w2 = (_ry - y0) * (x1 - _rx)  # y1, x0
                     w3 = (_ry - y0) * (_rx - x0)  # y1, x1
-
-                    return jnp.sum(
-                        jnp.array([p0 * w0, p1 * w1, p2 * w2, p3 * w3]))
+                    # embed()
+                    return jnp.sum(jnp.array(
+                        [p0 * w0, p1 * w1, p2 * w2, p3 * w3]),
+                                   axis=0)
 
                 _kernel_offsets = jnp.reshape(
                     _kernel_offsets, (self.filter_h, self.filter_w, 2))
+
                 # embed()
-                # _pixel(kernel_vs[0, 0], kernel_us[0, 0], _kernel_offsets[0, 0])
+                _pixel(kernel_vs[0, 0], kernel_us[0, 0], _kernel_offsets[0, 0])
                 return jax.vmap(jax.vmap(_pixel))(kernel_vs, kernel_us,
                                                   _kernel_offsets)
 
             # embed()
-            # _retrieve(vs[10, 0], us[10, 0], _offsets[10, 0])
+            _retrieve(vs[10, 0], us[10, 0], _image_offsets[10, 0])
             pixels = jax.vmap(jax.vmap(_retrieve))(vs, us, _image_offsets)
             return pixels
 
-        _volume = volume[0]
-        _offsets = offsets[0, ..., 0]
+        # _volume = volume[0]
+        # _offsets = offsets[0, ..., 0]
+        # y = _wrap(_volume, _offsets)
 
-        y = _wrap(_volume, _offsets)
-
-        def __wrap(_volume, _group_offsets):
+        def _batch_wrap(_volume, _offsets):
             # (2) Map over num_deform_groups dimension for offsets
-            return jax.vmap(_wrap, in_axes=(None, -1))(_volume, _group_offsets)
+            return jax.vmap(_wrap, in_axes=(None, -1), out_axes=(-1))(_volume,
+                                                                      _offsets)
 
-        # (1) Map over batch dimension for volume, offsets
-        return jax.vmap(__wrap)(volume, offsets)  # Batch
-        return y
+        # y = _batch_wrap(volume[0], offsets[0])
         # embed()
 
-        # Depth-wise convolution
+        # (1) Map over batch dimension for volume, offsets
+        pixels = jax.vmap(_batch_wrap)(volume, offsets)  # Batch
+        # embed()
+        """pixels are our pixel offsets for each image and deformable group.
+        pixels.shape = [batch_size, out_h, out_w, filter_h, filter_w, channel_in, num_deform_groups]
+        """
+        _pixels = pixels.transpose([0, 1, 3, 2, 4, 5, 6])
+        _pixels = jnp.reshape(
+            _pixels, (batch_size, out_h * self.filter_h, out_w * self.filter_w,
+                      self.num_deform_groups, channel_in))
 
-        # Add up
-        # return y
+        # Verify big feature reshape working correctly
+        # _a = pixels[0, 0, 0, :5, :5, 0, 0]
+        # _b = _pixels[0, :5, :5, 0, 0]
+        # embed()
+
+        # _pixels contains a set of offset pixels (depth = channel_in), one for each self.num_deform_group.
+        # We need to repeat each set of offset pixels by the size of each deformable group (features_per_group).
+
+        features_per_group = self.filters // self.num_deform_groups
+        # _pixels[batch, y, x, group_num, :] will now be of length features_per_group
+        _pixels = jnp.tile(_pixels, (1, 1, 1, 1, features_per_group))
+
+        # Flatten the last axis
+        _pixels = jnp.reshape(
+            _pixels,
+            (batch_size, out_h * self.filter_h, out_w * self.filter_w, -1))
+
+        # Perform depth-wise convolution
+        out_filters = self.filters * channel_in
+        out = nn.Conv(features=out_filters,
+                      kernel_size=self.kernel_size,
+                      feature_group_count=channel_in,
+                      strides=(self.filter_h, self.filter_w),
+                      padding=self.padding)(_pixels)
+        out = out.reshape(
+            (batch_size, out_h, out_w, channel_in, self.filters)).transpose(
+                (0, 1, 2, 4, 3))
+        out = jnp.sum(out, axis=-1)
+        return out
+        # embed()
 
 
-x_k, m_k = jax.random.split(jax.random.PRNGKey(0), 2)
-# N x H x W x C
-# C = D (maximum disparity)
-x = jax.random.uniform(x_k, (100, 64, 32, 10))
+if __name__ == "__main__":
+    x_k, m_k = jax.random.split(jax.random.PRNGKey(0), 2)
+    # N x H x W x C
+    # C = D (maximum disparity)
+    x = jax.random.uniform(x_k, (100, 64, 32, 10))
 
-model = DeformableConv(filters=32,
-                       kernel_size=(5, 5),
-                       num_deform_groups=2,
-                       kernel_dilation=(4, 2))
-variables = model.init(m_k, x)
+    model = DeformableConv(filters=32,
+                           kernel_size=(5, 5),
+                           num_deform_groups=2,
+                           kernel_dilation=(4, 2))
+    variables = model.init(m_k, x)
 
+    @jax.jit
+    def apply(variables, x):
+        y = model.apply(variables, x)
+        return y
 
-@jax.jit
-def apply(variables, x):
-    y = model.apply(variables, x)
-    return y
-
-
-y = apply(variables, x)
-embed()
+    y = apply(variables, x)
+    embed()
